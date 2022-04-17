876c8636a334a52c595ce8e37d0ad03eb662c960
André Kuhlmann <akuhltime@gmail.com>
2022-04-17 19:43:34 +0200

    Add $GITHUB_WORKSPACE variable

--- move-dist.sh
+++ move-dist.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mkdir dist-linux dist-mac dist-win > /dev/null 2>&1
+mkdir $GITHUB_WORKSPACE/dist-linux $GITHUB_WORKSPACE/dist-mac $GITHUB_WORKSPACE/dist-win > /dev/null 2>&1
 
-sudo mv dist_electron/latest-linux.yml dist_electron/*.AppImage dist_electron/*.tar.gz dist/*.snap dist-linux > /dev/null 2>&1
-sudo mv dist_electron/latest-mac.yml dist/*.dmg dist_electron/*.dmg.blockmap dist-mac > /dev/null 2>&1
-sudo mv dist_electron/latest.yml dist_electron/*.exe dist_electron/*.exe.blockmap dist-win > /dev/null 2>&1
+sudo mv $GITHUB_WORKSPACE/dist_electron/latest-linux.yml  $GITHUB_WORKSPACE/dist_electron/*.AppImage                $GITHUB_WORKSPACE/dist_electron/*.tar.gz       $GITHUB_WORKSPACE/dist/*.snap $GITHUB_WORKSPACE/dist-linux > /dev/null 2>&1
+sudo mv $GITHUB_WORKSPACE/dist_electron/latest-mac.yml    $GITHUB_WORKSPACE/dist/*.dmg dist_electron/*.dmg.blockmap $GITHUB_WORKSPACE/dist-mac                                                                                > /dev/null 2>&1
+sudo mv $GITHUB_WORKSPACE/dist_electron/latest.yml        $GITHUB_WORKSPACE/dist_electron/*.exe                     $GITHUB_WORKSPACE/dist_electron/*.exe.blockmap $GITHUB_WORKSPACE/dist-win                                 > /dev/null 2>&1
 
 exit 0
