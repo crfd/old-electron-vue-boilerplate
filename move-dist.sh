e535ba89504458772001a1d1996371fd80d5151a
André Kuhlmann <akuhltime@gmail.com>
2022-04-17 19:34:02 +0200

    Add sudo

--- move-dist.sh
+++ move-dist.sh
@@ -2,8 +2,8 @@
 
 mkdir dist-linux dist-mac dist-win > /dev/null 2>&1
 
-mv dist_electron/latest-linux.yml dist_electron/*.AppImage dist_electron/*.tar.gz dist/*.snap dist-linux > /dev/null 2>&1
-mv dist_electron/latest-mac.yml dist/*.dmg dist_electron/*.dmg.blockmap dist-mac > /dev/null 2>&1
-mv dist_electron/latest.yml dist_electron/*.exe dist_electron/*.exe.blockmap dist-win > /dev/null 2>&1
+sudo mv dist_electron/latest-linux.yml dist_electron/*.AppImage dist_electron/*.tar.gz dist/*.snap dist-linux > /dev/null 2>&1
+sudo mv dist_electron/latest-mac.yml dist/*.dmg dist_electron/*.dmg.blockmap dist-mac > /dev/null 2>&1
+sudo mv dist_electron/latest.yml dist_electron/*.exe dist_electron/*.exe.blockmap dist-win > /dev/null 2>&1
 
 exit 0
