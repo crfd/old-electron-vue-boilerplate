cbf3d0e213dbf6f252c00706dc073bda90ee5b9f
André Kuhlmann <akuhltime@gmail.com>
2022-04-18 11:39:26 +0000

    Cleanup move-dist.sh

--- move-dist.sh
+++ move-dist.sh
@@ -2,8 +2,8 @@
 
 mkdir ./dist-linux ./dist-mac ./dist-win
 
-mv ./dist_electron/latest-linux.yml  ./dist_electron/*.AppImage                ./dist_electron/*.tar.gz       ./dist/*.snap ./dist-linux
-mv ./dist_electron/latest-mac.yml    ./dist/*.dmg dist_electron/*.dmg.blockmap                                              ./dist-mac
-mv ./dist_electron/latest.yml        ./dist_electron/*.exe                     ./dist_electron/*.exe.blockmap               ./dist-win
+mv ./dist_electron/latest-linux.yml  ./dist_electron/*.AppImage ./dist_electron/*.tar.gz        ./dist/*.snap   ./dist-linux
+mv ./dist_electron/latest-mac.yml    ./dist/*.dmg               ./dist_electron/*.dmg.blockmap                  ./dist-mac
+mv ./dist_electron/latest.yml        ./dist_electron/*.exe      ./dist_electron/*.exe.blockmap                  ./dist-win
 
 exit 0
