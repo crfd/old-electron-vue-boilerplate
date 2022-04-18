996c3d638bff3df16576b848127cb86e71660f0f
André Kuhlmann <akuhltime@gmail.com>
2022-04-18 10:59:20 +0000

    Add logs

--- move-dist.sh
+++ move-dist.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mkdir ./dist-linux ./dist-mac ./dist-win > /dev/null 2>&1
+mkdir ./dist-linux ./dist-mac ./dist-win
 
-mv ./dist_electron/latest-linux.yml  ./dist_electron/*.AppImage                ./dist_electron/*.tar.gz       ./dist/*.snap ./dist-linux > /dev/null 2>&1
-mv ./dist_electron/latest-mac.yml    ./dist/*.dmg dist_electron/*.dmg.blockmap ./dist-mac                                                                                > /dev/null 2>&1
-mv ./dist_electron/latest.yml        ./dist_electron/*.exe                     ./dist_electron/*.exe.blockmap ./dist-win                 > /dev/null 2>&1
+mv ./dist_electron/latest-linux.yml  ./dist_electron/*.AppImage                ./dist_electron/*.tar.gz       ./dist/*.snap ./dist-linux
+mv ./dist_electron/latest-mac.yml    ./dist/*.dmg dist_electron/*.dmg.blockmap                                              ./dist-mac
+mv ./dist_electron/latest.yml        ./dist_electron/*.exe                     ./dist_electron/*.exe.blockmap               ./dist-win
 
 exit 0
